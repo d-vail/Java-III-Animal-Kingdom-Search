d528274fc4d14af0534e3f53232063ee1d5517ab
Danny Vail <danny_vail@outlook.com>
2019-02-06 19:36:19 -0500

    PERFORM SEARCHES WITH LAMBDA EXPRESSIONS In Main.java - search() loops through a given Animal ArrayList and tests the given conditions with a lambda expression - Perform searches requested in README

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -3,6 +3,13 @@ package com.lambdaschool;
 import java.util.ArrayList;
 
 public class Main {
+  private static void search(ArrayList<Animal> animals, Test conditions) {
+    for (Animal a : animals) {
+      if (conditions.test(a)) {
+        System.out.println(a.getName());
+      }
+    }
+  }
 
   public static void main(String[] args) {
     // TODO: Read in JSON data files
@@ -25,5 +32,34 @@ public class Main {
     animals.add(new Fish("Salmon", 1758));
     animals.add(new Fish("Catfish", 1817));
     animals.add(new Fish("Perch", 1758));
+
+    // TODO: Parse search queries from console
+    // Search using Lambda Expressions
+    System.out.println("\n*** List all the animals in descending order by year named ***");
+    animals.sort((a1, a2) -> a2.getYearNamed() - a1.getYearNamed());
+    animals.forEach(a -> System.out.println(a.getName() + ": { yearNamed: " + a.getYearNamed() + " }"));
+
+    System.out.println("\n*** List all the animals alphabetically ***");
+    animals.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+    animals.forEach(a -> System.out.println(a.getName()));
+
+    System.out.println("\n*** List all the animals ordered by how they move ***");
+    animals.sort((a1, a2) -> a1.move().compareToIgnoreCase(a2.move()));
+    animals.forEach(a -> System.out.println(a.getName() + ": { move: " + a.move() + " }"));
+
+    System.out.println("\n*** List only those animals the breath with lungs ***");
+    search(animals, a -> a.breath().equals("Lungs"));
+
+    System.out.println("\n*** List only those animals that breath with lungs and were named in " +
+            "1758" +
+            " ***");
+    search(animals, a -> a.breath().equals("Lungs") && a.getYearNamed() == 1758);
+
+    System.out.println("\n*** List only those animals that lay eggs and breath with lungs ***");
+    search(animals, a -> a.reproduce().equals("Eggs") && a.breath().equals("Lungs"));
+
+    System.out.println("\n*** List alphabetically only those animals that were named in 1758 ***");
+    animals.sort((a1, a2) -> a1.getName().compareToIgnoreCase(a2.getName()));
+    search(animals, a -> a.getYearNamed() == 1758);
   }
 }
